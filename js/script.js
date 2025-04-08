f16b10ee515c9b03d8211141e14a8a46fd5c84a4
sebastianvillegas25 <sebastianvillegas0628@gmail.com>
2025-04-07 22:38:08 -0500

    Add files via upload
    se añade el script de la pagina

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Añade este código a tu archivo JavaScript existente (script.js)
+
+// Referencias a elementos del DOM
+const botonesComprar = document.querySelectorAll('.comprar');
+const modalCompra = document.getElementById('modal-compra');
+const cerrarModalCompra = modalCompra.querySelector('.cerrar-modal');
+const productoTitulo = document.getElementById('producto-titulo');
+const productoPrecio = document.getElementById('producto-precio');
+
